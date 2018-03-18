7ae50ae9f4cd6dcdfd440d0478122d8c26bbaf00
Avi Warner <aviwarner@gmail.com>
2018-03-18 12:44:58 -0700

    static landing page content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Landing = () => (
   <section className="landing">
-    Landing page will go here
+    <h1 className="hero-title">Turn the music up!</h1>
+
+    <section className="selling-points">
+      <div className="point">
+        <h2 className="point-title">Choose your music</h2>
+        <p className="point-description">There's a ton of music out there so you should listen to music that you choose. Not music we choose. We don't even.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+        <p className="point-description">Unlimited in the sense that we'll have like... 3 albums.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Mobile enabled</h2>
+        <p className="point-description">Listen to your music on the go. Walking, on a train, in a plane. While eating green eggs & ham.</p>
+      </div>
+    </section>
   </section>
 );
 
